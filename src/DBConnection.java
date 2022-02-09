7c4596a1fbaaf498e3cb0b1a9c49e6f61f888cc7
bila-alt <zergougnabila@gmail.com>
2022-02-09 19:29:59 +0100

    la question 2 et 3 le patron singleton dans la classe « DBConnection »

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -2,25 +2,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 /*commentaire*/
+
+/* to implement the singleton pattern: j'ai changer l'accée du constructeur de publique (public) a privé (private)
+ * En plus j'ai ajouté une instance 'instance' et j'ai ajouté la methode 'getInstance()' qui retourne 'instance'*/
+
 public class DBConnection {
 	   
-		String BDD = "nomBD";
+		String BDD = "bibliothèque numérique";
 		String url = "jdbc:mysql://localhost:3306/" + BDD;
 		String user = "root";
 		String passwd = "";
-	    private Connection conn;
-
+	    private static Connection conn;
+	    private static DBConnection instance;
 	   
 	    public DBConnection() throws SQLException {
 			conn=DriverManager.getConnection(url, user,passwd);
 		}
-
 	    
 	    public Connection getConn() {
 			return conn;
 		}
+	    
+	    public static DBConnection getInstance() throws SQLException {
+	    	if (instance == null)
+	    	{ 
+	    		instance = new DBConnection ();
+	    		conn = instance.getConn();
+	    	}
+			return instance;
+		}
 
-
-		
-	
 }
+
