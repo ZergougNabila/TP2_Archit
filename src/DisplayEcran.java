f62757c392299bd3dfb77cba8f70c79c5d925415
bila-alt <zergougnabila@gmail.com>
2022-02-09 20:30:12 +0100

    la question 7 et 8 et 9

---
+++ src/DisplayEcran.java
@@ -0,0 +1,12 @@
+
+public class DisplayEcran  implements IJournal{
+
+	@Override
+	public void outPut_Msg(String message) {
+		
+      System.out.println(message);
+	}
+		
+}
+
+
--- src/IEtdSrv.java
+++ src/IEtdSrv.java
@@ -1,5 +1,7 @@
 import java.sql.SQLException;
-
+//justification :
+/* d'aprés le principe d'inversion de controle les modules de haut niveau 
+*ne doivent pas dependre des modules de bas niveau.*/
 public interface IEtdSrv {
 	boolean inscription (int matricule, String nom, String prénom, String email,String pwd, int id_universite) throws SQLException;
 
